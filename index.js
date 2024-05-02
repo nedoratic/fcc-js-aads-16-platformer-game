ef509eb83525fd2f10e15586b1da153b55a309c0
nedoratic <nedoratic@gmail.com>
2024-05-02 07:53:19 +0200

    index.js - checkpoint class added

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ class Player {
 	}
 }
 
+// Platform Class
 class Platform {
 	constructor(x, y) {
 		this.position = {
@@ -75,5 +76,29 @@ class Platform {
 	draw() {
 		ctx.fillStyle = '#acd157';
 		ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+	}
+}
+
+// CheckPoint Class
+class CheckPoint {
+	constructor(x, y, z) {
+		this.position = {
+			x,
+			y,
+		};
+		this.width = proportionalSize(40);
+		this.height = proportionalSize(70);
+		this.claimed = false;
+	}
+
+	draw() {
+		ctx.fillStyle = '#f1be32';
+		ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+	}
+	claim() {
+		this.width = 0;
+		this.height = 0;
+		this.position.y = Infinity;
+		this.claimed = true;
 	}
 }
