d7ed49f54f72897adead58903be3a668c7ef085f
nedoratic <nedoratic@gmail.com>
2024-05-02 07:50:24 +0200

    basic variables added

---
+++ index.js
@@ -0,0 +1,20 @@
+// DOM Elements Variables
+const startScreen = document.querySelector('.start-screen');
+const startBtn = document.getElementById('start-btn');
+const checkpointScreen = document.querySelector('.checkpoint-screen');
+const checkpointMessage = document.querySelector('.checkpoint-screen > p');
+
+// Canvas Variables
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+// Game
+const gravity = 0.5;
+
+let isCheckpointCollisionDetectionActive = true;
+
+const proportionalSize = (size) => {
+	return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
+};
